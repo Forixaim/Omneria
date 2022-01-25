26abd881000c4a222b4bf3b6bca31b2ad4ac9574
Forixaim <thebest20067@gmail.com>
2022-01-24 21:09:25 -0800

    Add files via upload

---
+++ src/main/java/com/forixaim/omneria/Omneria.java
@@ -0,0 +1,15 @@
+package com.forixaim.omneria;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Omneria.MOD_ID)
+public class Omneria
+{
+	public static final String MOD_ID = "forixaim_omneria";
+    public Omneria() {
+       
+    }
+}
